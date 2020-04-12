798b05d10f5d1540a2de2198b49f8143935f2df0
Artur <hafud@mail.ru>
2020-04-12 21:00:50 +0400

    host

--- src/scripts/isBgNavigation.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  const nav = document.querySelector('.navigation');
-
-  if (window.pageYOffset >= 800) {
-    nav.classList.add('navigation--background');
-  } else {
-    nav.classList.remove('navigation--background');
-  }
-};
