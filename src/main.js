c4ad03aa00e062c5fba2ab77e98175d64895dcca
Artur Papikian <artur.jog@yandex.ru>
2020-05-16 22:07:17 +0300

    add animation block works

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-new */
+import HoverEffect from 'hover-effect'
 import initSlider from './blocks/twitter/index';
 import scrollingSection from './scripts/scrollSection';
 import parallax from './scripts/parallax';
@@ -11,3 +13,19 @@ initSlider();
 scrollingSection();
 parallax();
 toggleMenu();
+
+Array.from(document.querySelectorAll('.work__bike-index')).forEach(item => {
+  const images = item.querySelectorAll('.work__hidden');
+
+  new HoverEffect({
+    parent: item,
+    image1: `${images[0].src}`,
+    image2: `${images[1].src}`,
+    speedIn: 1,
+    speedOut: 0.8,
+    intensity: 0.5,
+    angle: 15,
+    displacementImage: item.dataset.displacement,
+    imagesRatio: item.dataset.height / item.dataset.width,
+  });
+});
