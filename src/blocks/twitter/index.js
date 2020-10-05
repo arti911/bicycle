e4a2575250ef10dad2fa02f1a0c186e866ec12a4
Artur <artur.jog@yandex.ru>
2020-10-05 15:34:33 +0400

    Upgrade: package.json & init swiper

--- src/blocks/twitter/index.js
+++ src/blocks/twitter/index.js
@@ -1,11 +1,14 @@
-import Swiper from 'swiper';
+import Swiper, { Pagination } from 'swiper';
 
 const initSlider = () => {
+  Swiper.use([Pagination]);
+
   return new Swiper('.swiper-container', {
     pagination: {
       el: '.swiper-pagination',
+      type: 'bullets',
     },
-  });
+  })
 };
 
 export default initSlider;
